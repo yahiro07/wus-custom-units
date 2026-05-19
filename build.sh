d7e7ee0b0cb76f4a09f085c089da41f3ac751165
yahiro <yahiro07@users.noreply.github.com>
2026-05-19 13:16:31 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 dstDir=./dist
-rm -rf $dstDir
-mkdir -p $dstDir
-cd ./koodori && npm run build && cd ..
-cd ./bc-010 && npm run build && cd ..
-cp -r koodori/dist $dstDir/koodori
-cp -r bc-010/build $dstDir/bc-010
-cp -r additive/web $dstDir/additive
-cp -r drum-machine/web $dstDir/drum-machine
-cp -r wasyn-1/web $dstDir/wasyn-1
-cp -r webaudio-synth-v2/web $dstDir/webaudio-synth-v2
-cp -r webaudio-tinysynth-simple/web $dstDir/webaudio-tinysynth-simple
+rm -rf "$dstDir"
+mkdir -p "$dstDir"
+cd ./koodori && npm install && npm run build && cd ..
+cd ./bc-010 && npm install && npm run build && cd ..
+cp -r koodori/dist "$dstDir/koodori"
+cp -r bc-010/build "$dstDir/bc-010"
+cp -r additive/web "$dstDir/additive"
+cp -r drum-machine/web "$dstDir/drum-machine"
+cp -r wasyn-1/web "$dstDir/wasyn-1"
+cp -r webaudio-synth-v2/web "$dstDir/webaudio-synth-v2"
+cp -r webaudio-tinysynth-simple/web "$dstDir/webaudio-tinysynth-simple"
 echo "copying done."
