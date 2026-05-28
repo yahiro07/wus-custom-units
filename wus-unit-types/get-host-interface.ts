7493716d4d6b2dbfda6f4441d47b17b630c06dc8
yahiro <yahiro07@users.noreply.github.com>
2026-05-28 15:05:20 +0900

    refer wus-unit-types on npm

--- wus-unit-types/get-host-interface.ts
+++
@@ -1,8 +0,0 @@
-import { HostInterface } from "./unit-interfaces-2";
-
-export function getHostInterface(): HostInterface | undefined {
-  type WindowWithHostInterface = {
-    hostInterface?: HostInterface;
-  };
-  return (window as WindowWithHostInterface)?.hostInterface;
-}
--- wus-unit-types/unit-metadata.ts
+++
@@ -1,9 +0,0 @@
-import { UnitCategoryHint, UnitType } from "./unit-interfaces-2";
-
-export type UnitMetadata = {
-  unitType: UnitType;
-  name: string;
-  repositoryUrl: string;
-  category?: UnitCategoryHint;
-  preferredSize: string;
-};
