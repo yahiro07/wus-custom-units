1e37da0eee87e1d868441efb04b800be859b1d69
yahiro <yahiro07@users.noreply.github.com>
2026-06-21 01:02:45 +0900

    add build_single.sh

---
+++ build_single.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dstDir=./dist
+mkdir -p "$dstDir"
+
+# opt in rows as you need
+
+# cd ./koodori && npm install && npm run build && cd ..
+# cd ./bc-010 && npm install && npm run build && cd ..
+# cd ./threejs-audio-reactive-visual && npm install && npm run build && cd ..
+# cp -r koodori/dist/. "$dstDir/koodori"
+# cp -r bc-010/build/. "$dstDir/bc-010"
+# cp -r threejs-audio-reactive-visual/dist/. "$dstDir/threejs-audio-reactive-visual"
+# cp -r additive/web/. "$dstDir/additive"
+# cp -r drum-machine/web/. "$dstDir/drum-machine"
+# cp -r wasyn-1/web/. "$dstDir/wasyn-1"
+# cp -r webaudio-synth-v2/web/. "$dstDir/webaudio-synth-v2"
+# cp -r webaudio-tinysynth-simple/web/. "$dstDir/webaudio-tinysynth-simple"
+# cp -r vissonance/web/. "$dstDir/vissonance"
+
+echo "copying done."
