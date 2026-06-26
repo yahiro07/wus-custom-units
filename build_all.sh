8ba16b8a84d3f2f4ae7da0d6abcfa8d8444916e5
yahiro <yahiro07@users.noreply.github.com>
2026-06-27 04:21:22 +0900

    update build script

--- build_all.sh
+++ build_all.sh
@@ -5,14 +5,9 @@ set -euo pipefail
 dstDir=./dist
 rm -rf "$dstDir"
 mkdir -p "$dstDir"
-cd ./koodori && npm install && npm run build && cd ..
-cd ./bc-010 && npm install && npm run build && cd ..
 cd ./threejs-audio-reactive-visual && npm install && npm run build && cd ..
-cp -r koodori/dist "$dstDir/koodori"
-cp -r bc-010/build "$dstDir/bc-010"
 cp -r threejs-audio-reactive-visual/dist "$dstDir/threejs-audio-reactive-visual"
 cp -r additive/web "$dstDir/additive"
-cp -r drum-machine/web "$dstDir/drum-machine"
 cp -r wasyn-1/web "$dstDir/wasyn-1"
 cp -r webaudio-synth-v2/web "$dstDir/webaudio-synth-v2"
 cp -r webaudio-tinysynth-simple/web "$dstDir/webaudio-tinysynth-simple"
--- build_single.example.sh
+++ build_single.example.sh
@@ -7,14 +7,9 @@ mkdir -p "$dstDir"
 
 # opt in rows as you need
 
-# cd ./koodori && npm install && npm run build && cd ..
-# cd ./bc-010 && npm install && npm run build && cd ..
 # cd ./threejs-audio-reactive-visual && npm install && npm run build && cd ..
-# cp -r koodori/dist/. "$dstDir/koodori"
-# cp -r bc-010/build/. "$dstDir/bc-010"
 # cp -r threejs-audio-reactive-visual/dist/. "$dstDir/threejs-audio-reactive-visual"
 # cp -r additive/web/. "$dstDir/additive"
-# cp -r drum-machine/web/. "$dstDir/drum-machine"
 # cp -r wasyn-1/web/. "$dstDir/wasyn-1"
 # cp -r webaudio-synth-v2/web/. "$dstDir/webaudio-synth-v2"
 # cp -r webaudio-tinysynth-simple/web/. "$dstDir/webaudio-tinysynth-simple"
